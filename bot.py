740236bd0e20cb43bde56e51afe862866108ad06
LadaBelan <Lada.cotnik@gmail.com>
2025-11-20 01:46:26 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(name)
 
 
 def get_main_keyboard() -> InlineKeyboardMarkup:
